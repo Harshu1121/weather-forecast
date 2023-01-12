a1f3a5ccfbbf09c125ee9535f48efaa8b37aec77
Harshvardhan <harshvardhanpasalkar@gmail.com>
2023-01-12 14:43:25 +0530

    adding backend

---
+++ backend.py
@@ -0,0 +1,14 @@
+import requests
+
+API_KEY = "e1f4f8e145f873c282bb767284038d00"
+
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
