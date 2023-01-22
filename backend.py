46cce56eeedefb4fa9fcceb1fc62d5cffffe2e69
Harshvardhan <harshvardhanpasalkar@gmail.com>
2023-01-22 13:40:33 +0530

    adding temperature unit in main and adding error handeling in backend

--- backend.py
+++ backend.py
@@ -1,17 +1,38 @@
 import requests
+import requests_cache
 
 API_KEY = "e1f4f8e145f873c282bb767284038d00"
 
-
-def get_data(place, forecast_days=None):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
-    response = requests.get(url)
-    data = response.json()
-    filtered_data = data["list"]
-    nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    return filtered_data
+# Set up cache for requests
+requests_cache.install_cache('weather_cache', expire_after=3600)
 
 
-if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3))
+def get_data(place, forecast_days=None, units='metric', lang='en'):
+    try:
+        # validate API key
+        if not API_KEY:
+            raise ValueError("API key is not set.")
+
+        # API url
+        url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units={units}&lang={lang}"
+
+        # Make GET request to API
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        filtered_data = data["list"]
+
+        # Filter data by number of forecast days
+        if forecast_days:
+            nr_values = 8 * forecast_days
+            filtered_data = filtered_data[:nr_values]
+
+        return filtered_data
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("Something went wrong:", err)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number forecasted days")
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
+unit = st.selectbox("Temperature Unit", ("Celsius", "Fahrenheit"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
@@ -18,9 +19,11 @@ if place:
 
         if option == "Temperature":
             temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            if unit == "Fahrenheit":
+                temperatures = [temp * 9/5 + 32 for temp in temperatures]
             dates = [dict["dt_txt"] for dict in filtered_data]
             # Create a temperature plot
-            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": f"Temperature ({unit})"})
             st.plotly_chart(figure)
 
         if option == "Sky":
@@ -28,7 +31,11 @@ if place:
                       "Snow": "images/snow.png"}
             sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
             image_paths = [images[condition] for condition in sky_conditions]
-            print(sky_conditions)
-            st.image(image_paths, width=115)
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            st.write("Image"+"\t\t"+"Date"+"\t\t\t"+"Time")
+            for i in range(len(image_paths)):
+                st.write("")
+                st.image(image_paths[i], width=50)
+                st.write("\t\t"+dates[i].split(" ")[0]+"\t\t\t"+dates[i].split(" ")[1])
     except KeyError:
         st.write("That place does not exists...")
